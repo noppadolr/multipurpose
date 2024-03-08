57194cfb54c15446aac5af5d81797636694a9247
noppadolr <riyasarn.n@gmail.com>
2024-03-08 16:59:52 +0700

    x-component

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'username',
+        'phone',
+        'photo',
+        'address',
         'email',
         'password',
     ];
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name'=>'Admin',
+                'username'=>'admin',
+                'email'=>'admin@gmail.com',
+                'password'=>Hash::make('111'),
+                'role'=>'admin',
+                'status'=>'active',
+                'created_at'=>Carbon::now(),
+                'email_verified_at'=>Carbon::now(),
+            ],
+            [
+                'name'=>'User',
+                'username'=>'user',
+                'email'=>'user@gmail.com',
+                'password'=>Hash::make('111'),
+                'role'=>'user',
+                'status'=>'active',
+                'created_at'=>Carbon::now(),
+                'email_verified_at'=>Carbon::now(),
+
+            ]
+
+        ]);
+    }
+}
